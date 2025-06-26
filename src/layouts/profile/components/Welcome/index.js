4b3568d58bf590b7d0eba8292a3ace7964817540
ScottRCSI <scott.reitzel@mckesson.com>
2025-06-26 08:35:32 -0700

    Update index.js

--- src/layouts/profile/components/Welcome/index.js
+++ src/layouts/profile/components/Welcome/index.js
@@ -41,7 +41,7 @@ const Welcome = () => {
             Welcome back!
           </VuiTypography>
           <VuiTypography color="white" variant="button" fontWeight="regular">
-            Nice to see you, Mark Johnson!
+            Nice to see you, Scott!
           </VuiTypography>
         </VuiBox>
         <VuiBox justifySelf="flex-end">
