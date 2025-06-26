112d178e4ac98a3f15150407f20f74220f825e7d
ScottRCSI <scott.reitzel@mckesson.com>
2025-06-26 08:54:53 -0700

    Update index.js

--- src/layouts/dashboard/components/WelcomeMark/index.js
+++ src/layouts/dashboard/components/WelcomeMark/index.js
@@ -1,29 +1,49 @@
 import React from "react";
-
 import { Card, Icon } from "@mui/material";
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
-
 import gif from "assets/images/cardimgfree.png";
 
 const WelcomeMark = () => {
   return (
-    <Card sx={() => ({
-      height: "340px",
-      py: "32px",
-      backgroundImage: `url(${gif})`,
-      backgroundSize: "cover",
-      backgroundPosition: "50%"
-    })}>
-      <VuiBox height="100%" display="flex" flexDirection="column" justifyContent="space-between">
+    <Card
+      sx={{
+        height: "340px",
+        py: "32px",
+        backgroundImage: `url(${gif})`,
+        backgroundSize: "cover",
+        backgroundPosition: "50%",
+      }}
+    >
+      <VuiBox
+        height="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
         <VuiBox>
-          <VuiTypography color="text" variant="button" fontWeight="regular" mb="12px">
+          <VuiTypography
+            color="text"
+            variant="button"
+            fontWeight="regular"
+            mb="12px"
+          >
             Welcome back,
           </VuiTypography>
-          <VuiTypography color="white" variant="h3" fontWeight="bold" mb="18px">
+          <VuiTypography
+            color="white"
+            variant="h3"
+            fontWeight="bold"
+            mb="18px"
+          >
             Scott
           </VuiTypography>
-          <VuiTypography color="text" variant="h6" fontWeight="regular" mb="auto">
+          <VuiTypography
+            color="text"
+            variant="h6"
+            fontWeight="regular"
+            mb="auto"
+          >
             Glad to see you again!
             <br /> Ask me anything.
           </VuiTypography>
@@ -39,14 +59,12 @@ const WelcomeMark = () => {
             display: "inline-flex",
             alignItems: "center",
             cursor: "pointer",
-
             "& .material-icons-round": {
               fontSize: "1.125rem",
               transform: `translate(2px, -0.5px)`,
               transition: "transform 0.2s cubic-bezier(0.34,1.61,0.7,1.3)",
             },
-
-            "&:hover .material-icons-round, &:focus  .material-icons-round": {
+            "&:hover .material-icons-round, &:focus .material-icons-round": {
               transform: `translate(6px, -0.5px)`,
             },
           }}
@@ -60,3 +78,4 @@ const WelcomeMark = () => {
 };
 
 export default WelcomeMark;
+
